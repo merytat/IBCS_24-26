e9bb1c847ef3ce247c726ca361f0ae8fdca041f5
Mery Tellez <76423201+merytat@users.noreply.github.com>
2024-10-30 14:03:14 +0300

    Trail class

--- src/UnitP2/Trail.java
+++ src/UnitP2/Trail.java
@@ -6,6 +6,8 @@ public class Trail {
     private double pointA;
     private double pointB;
     private static int trailCounter = 0;
+    private static double totalDistance = 0;
+    private static String warning;
 
     //constructor
     public Trail(String n , double pa, double pb){
@@ -13,6 +15,15 @@ public class Trail {
         this.pointA = pa;
         this.pointB = pb;
         trailCounter++;
+        totalDistance += getDistance();
+    }
+
+    public Trail(){
+        this.name = "Magnolia";
+        this.pointB = 150;
+        this.pointA = 0;
+        trailCounter++;
+        totalDistance += getDistance();
     }
 
     //create an ID
@@ -27,10 +38,47 @@ public class Trail {
         return id.toUpperCase();
     }
 
+    public double getDistance(){
+        return pointB-pointA;
+    }
+
+    public double getMidPoint(){
+        return (pointB+pointA)/2;
+    }
+
+    public double getRandomPoint(){
+        System.out.println(pointA);
+        return (Math.random()*(pointB-pointA))+pointA;
+    }
+
+    public static int getTrailCount(){
+        return trailCounter;
+    }
+
+    public static double getDistanceOfTrails(){
+        return totalDistance;
+    }
+
+    public static double averageDistanceOfTrails(){
+        return totalDistance/trailCounter;
+    }
+
+    public static void sendWarning(String message){
+        warning = "WARNING: " + message + "!";
+    }
+
+    public String getWarning(){
+        return warning;
+    }
+
     //toString to print an object
     public String toString(){
         String result = "Trail #" + trailCounter + "\n";
         result = result + "Name: " + name + "\n";
+        result += "TrailID: " + generateID() + "\n";
+        result += "PointA: " + pointA + "\n";
+        result += "PointB: " + pointB + "\n";
+        result += "Trail Distance: " + getDistance();
         return result;
 //        Trail #1
 //        Name: Mahogany
--- src/UnitP2/TrailTester.java
+++ src/UnitP2/TrailTester.java
@@ -7,40 +7,40 @@ public class TrailTester
         Trail t1 = new Trail("Mahogany", 150.50, 300.50);
 
         System.out.println(t1);
-        //System.out.println("MidPoint: " + t1.getMidPoint());
-        //System.out.println("Random Point: " + t1.getRandomPoint());
+        System.out.println("MidPoint: " + t1.getMidPoint());
+        System.out.println("Random Point: " + t1.getRandomPoint());
 
-//        System.out.println("----------------------------------");
-//
-//        Trail t2 = new Trail("Cheesy", 452.12, 125.56);
-//        System.out.println(t2);
-//        System.out.println("MidPoint: " + t2.getMidPoint());
-//        System.out.println("Random Point: " + t1.getRandomPoint());
-//
-//        System.out.println("----------------------------------");
-//
-//        Trail t3 = new Trail();
-//        System.out.println(t3);
-//        System.out.println("MidPoint: " + t3.getMidPoint());
-//        System.out.println("Random Point: " + t1.getRandomPoint());
-//
-//        System.out.println("----------------------------------");
-//
-//
-//
-//        System.out.println("Number of trails created: " + Trail.getTrailCount());
-//        System.out.println("Total Distance of trails created: " + Trail.getDistanceOfTrails());
-//        System.out.println("Average distance of " + Trail.getTrailCount() + " trails: " +
-//                Trail.averageDistanceOfTrails());
-//
-//        System.out.println("----------------------------------");
-//
-//        System.out.println("------Sending Trail Warning--------");
-//
-//        Trail.sendWarning("There is a landslide at a slope 5km from Trail #3");
-//        System.out.println(t1.getWarning());
-//        System.out.println(t2.getWarning());
-//        System.out.println(t3.getWarning());
+        System.out.println("----------------------------------");
+
+        Trail t2 = new Trail("Cheesy", 452.12, 125.56);
+        System.out.println(t2);
+        System.out.println("MidPoint: " + t2.getMidPoint());
+        System.out.println("Random Point: " + t2.getRandomPoint());
+
+        System.out.println("----------------------------------");
+
+        Trail t3 = new Trail();
+        System.out.println(t3);
+        System.out.println("MidPoint: " + t3.getMidPoint());
+        System.out.println("Random Point: " + t3.getRandomPoint());
+
+        System.out.println("----------------------------------");
+
+
+
+        System.out.println("Number of trails created: " + Trail.getTrailCount());
+        System.out.println("Total Distance of trails created: " + Trail.getDistanceOfTrails());
+        System.out.println("Average distance of " + Trail.getTrailCount() + " trails: " +
+                Trail.averageDistanceOfTrails());
+
+        System.out.println("----------------------------------");
+
+        System.out.println("------Sending Trail Warning--------");
+
+        Trail.sendWarning("There is a landslide at a slope 5km from Trail #3");
+        System.out.println(t1.getWarning());
+        System.out.println(t2.getWarning());
+        System.out.println(t3.getWarning());
 
     }
 }
